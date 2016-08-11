db7883edd8007d14ce4f136f93c6c96dc11e6bc4
Mirza Adipradhana <seestern.it@gmail.com>
2016-08-12 00:31:43 +0700

    Implement Button UI on index page

--- src/client/ui/views/PageIndex/PageIndex.jsx
+++ src/client/ui/views/PageIndex/PageIndex.jsx
@@ -1,5 +1,6 @@
-import styles from './style.css';
 import logo from '~/src/client/assets/images/logo-dark.png';
+import Button from '~/src/client/ui/components/Button';
+import styles from './style.css';
 
 class PageIndex extends React.Component {
   constructor(props) {
@@ -11,6 +12,7 @@ class PageIndex extends React.Component {
       <div className={styles.root}>
         <img src={logo} alt="logo" />
         <div>This is from react</div>
+        <Button className={styles.button}>Hallo My Button</Button>
       </div>
     );
   }
