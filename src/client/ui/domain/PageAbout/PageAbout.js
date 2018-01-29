4c51811f22903f517ddd01211483d030ba24523a
Mirza Adipradhana <madipradhana@gmail.com>
2018-01-29 12:33:39 +0700

    add micro interaction on menu and remove unused component and style

--- src/client/ui/domain/PageAbout/PageAbout.js
+++ src/client/ui/domain/PageAbout/PageAbout.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import styles from './style.css';
 import Content from '../../uikit/sections/Content';
 
 class PageAbout extends React.PureComponent {
   render() {
     return (
-      <div className={styles.root}>
-        <Content verticalAlign="middle">
-          <h1 style={{ textAlign: 'center' }}>Hi, I'm Mirza Adipradhana</h1>
-        </Content>
-      </div>
+      <Content verticalAlign="middle">
+        <h1 style={{ textAlign: 'center' }}>Hi, I'm Mirza Adipradhana</h1>
+      </Content>
     );
   }
 }
