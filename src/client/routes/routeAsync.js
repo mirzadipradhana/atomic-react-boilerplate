308a7cb656e566c71eabb9a5346e3c3b15f484a6
Mirza Adipradhana <madipradhana@gmail.com>
2018-01-29 12:11:57 +0700

    add styling on placeholder

--- src/client/routes/routeAsync.js
+++ src/client/routes/routeAsync.js
@@ -3,3 +3,4 @@ import RouteAsyncContainer from './routeAsyncContainer';
 
 export const IndexPage = () => <RouteAsyncContainer load={import('../ui/domain/PageIndex')} />;
 export const AboutPage = () => <RouteAsyncContainer load={import('../ui/domain/PageAbout')} />;
+export const NotFoundPage = () => <RouteAsyncContainer load={import('../ui/domain/NotFound')} />;
